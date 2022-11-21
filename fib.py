7b90cd8af210fca40e3eac4b5f984d473b2b56e7
Bishakha18 <84800573+Bishakha18@users.noreply.github.com>
2022-11-21 18:35:51 +0530

    fibonacci

---
+++ fib.py
@@ -0,0 +1,19 @@
+def febb(n):
+    if n==0 :
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return febb(n-1)+febb(n-2)
+    
+
+
+n=int(input("enter the range of fibnacci series: "))
+p=0
+for i in range(n):
+    p= febb(i)
+    if i == n-1:
+        print(f"{p} ",end ="")
+        break
+    print(f"{p}, ",end ="")
+        
